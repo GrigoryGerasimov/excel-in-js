c5f73d6f9fb68bdc9c9d232e6445bb9261e8c9bc
Grigory <rehor.ger@gmail.com>
2023-01-20 20:50:54 +0100

    table class decomposition done, static props and methods and ancestor init moved to separate modules

--- src/app/class.components/Table/TableEventHandlers.js
+++ src/app/class.components/Table/TableEventHandlers.js
@@ -1,46 +1,12 @@
-import { EventHandler } from "@framework/EventHandler";
 import { setResizedDimensions } from "@framework/utils/dom.operations/setResizedDimensions";
+import { StaticMixinTable } from "./table.mixins/StaticMixinTable";
+import { initAncestor } from "./table.utils/initAncestor";
+import { EventHandler } from "@framework/EventHandler";
 import { $ } from "@framework/CoreDOM";
-import { ErrorDOM } from "@framework/utils/errors/ErrorDOM";
-import { cachingWrapperDOM } from "@framework/utils/decorator/cachingWrapperDOM";
 
 export class TableEventHandlers extends EventHandler {
-    static ancestor = null;
-
-    static get colCellCollection() {
-        const getColCellCollection = $ancestor => $ancestor ? EventHandler.self.$rootElem.parent.querySelectorAll(`[data-colcode="${$ancestor.parent.dataset.colcode}"]`) : new ErrorDOM("Please provide an ancestor for dataset colcodes").throw();
-        return cachingWrapperDOM(getColCellCollection, TableEventHandlers.ancestor)();
-    }
-
-    static defineResizersForColCells(resizeStyles) {
-        return function(colCell) {
-            return TableEventHandlers.ancestor?.defineResizers({ resizeAncestor: colCell, resizeType: "col", action: "show", resizeStyles });
-        };
-    }
-
-    static iterateColCellCollection(fn) {
-        const iteratorWrapper = () => { TableEventHandlers.colCellCollection.forEach(fn); };
-        return cachingWrapperDOM(iteratorWrapper)();
-    }
-
-    static showColResizers(resizeStyles) {
-        TableEventHandlers.iterateColCellCollection(TableEventHandlers.defineResizersForColCells(resizeStyles));
-    }
-
-    static showRowResizers(resizeStyles) {
-        TableEventHandlers.ancestor?.defineResizers({ resizeType: "row", action: "show", resizeStyles });
-    }
-
-    static hideResizers(resizeStyles) {
-        TableEventHandlers.ancestor?.defineResizers({ resizeAncestor: EventHandler.self.$rootElem.parent, action: "hide", resizeStyles });
-    }
-
     onMousedown(evt) {
-        if (evt.target.dataset.resize === "col") {
-            TableEventHandlers.ancestor = $(evt.target).ancestor(`[data-colcode]`);
-        } else if (evt.target.dataset.resize === "row") {
-            TableEventHandlers.ancestor = $(evt.target).ancestor(`[data-type="row"]`);
-        }
+        TableEventHandlers.ancestor = initAncestor(evt.target);
 
         document.onmousemove = this.onMousemove;
         document.onmouseup = this.onMouseup;
@@ -48,9 +14,15 @@ export class TableEventHandlers extends EventHandler {
 
     onMousemove(evt) {
         if (TableEventHandlers.ancestor?.parent?.hasAttribute("data-colcode")) {
-            TableEventHandlers.showColResizers({ opacity: "0.5", right: -$(TableEventHandlers.ancestor).computeMouseDelta(evt.pageX).mouseDifferX + "px" });
+            TableEventHandlers.showColResizers({
+                right: -TableEventHandlers.ancestor.computeMouseDelta(evt.pageX).mouseDifferX + "px",
+                opacity: "0.5"
+            });
         } else if (TableEventHandlers.ancestor?.parent?.dataset.type === "row") {
-            TableEventHandlers.showRowResizers({ opacity: "0.5", bottom: -$(TableEventHandlers.ancestor).computeMouseDelta(evt.pageY).mouseDifferY + "px" });
+            TableEventHandlers.showRowResizers({
+                bottom: -TableEventHandlers.ancestor.computeMouseDelta(evt.pageY).mouseDifferY + "px",
+                opacity: "0.5"
+            });
         }
         return false;
     }
@@ -69,9 +41,15 @@ export class TableEventHandlers extends EventHandler {
         }
 
         document.onmousemove = null;
-        TableEventHandlers.hideResizers({ opacity: "0", right: "0", bottom: "0" });
+        TableEventHandlers.hideResizers({
+            opacity: "0",
+            right: "0",
+            bottom: "0"
+        });
         TableEventHandlers.ancestor = null;
         document.onmouseover = e => { if (e.target.dataset.resize) $(e.target).css({ opacity: "1" }); };
         document.onmouseout = event => { if (event.target.dataset.resize) $(event.target).css({ opacity: "0" }); };
     }
 }
+
+Object.assign(TableEventHandlers, StaticMixinTable(EventHandler));
---
+++ src/app/class.components/Table/table.mixins/StaticMixinTable.js
@@ -0,0 +1,41 @@
+import { cachingWrapperDOM } from "@framework/utils/decorator/cachingWrapperDOM";
+import { ErrorDOM } from "@framework/utils/errors/ErrorDOM";
+
+export const StaticMixinTable = parentClass => ({
+    ancestor: null,
+    colCellCollection() {
+        const getColCellCollection = $ancestor => $ancestor ? parentClass.self.$rootElem.parent.querySelectorAll(`[data-colcode="${$ancestor.parent.dataset.colcode}"]`) : new ErrorDOM("Please provide an ancestor for dataset colcodes").throw();
+        return cachingWrapperDOM(getColCellCollection, this.ancestor)();
+    },
+    defineResizersForColCells(resizeStyles) {
+        return colCell => {
+            return this.ancestor?.defineResizers({
+                resizeAncestor: colCell,
+                resizeType: "col",
+                action: "show",
+                resizeStyles
+            });
+        };
+    },
+    iterateColCellCollection(fn) {
+        const iteratorWrapper = () => { this.colCellCollection().forEach(fn); };
+        return cachingWrapperDOM(iteratorWrapper)();
+    },
+    showColResizers(resizeStyles) {
+        this.iterateColCellCollection(this.defineResizersForColCells(resizeStyles));
+    },
+    showRowResizers(resizeStyles) {
+        this.ancestor?.defineResizers({
+            resizeType: "row",
+            action: "show",
+            resizeStyles
+        });
+    },
+    hideResizers(resizeStyles) {
+        this.ancestor?.defineResizers({
+            resizeAncestor: parentClass.self.$rootElem.parent,
+            action: "hide",
+            resizeStyles
+        });
+    }
+});
