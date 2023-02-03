76fd4fa17ddbe4f70e12d3d215bd2e1cbbcd1891
Grigory <rehor.ger@gmail.com>
2023-02-03 09:39:41 +0100

    console.log removed

--- src/app/class.components/Toolbar/toolbar.components/toolbar.body.js
+++ src/app/class.components/Toolbar/toolbar.components/toolbar.body.js
@@ -2,7 +2,6 @@ import { toolbarIcons } from "./toolbar.icons";
 import { wrapSVG } from "./toolbar.svg";
 
 export const createToolbarBody = state => {
-    console.log(state);
     return `
     <div class="toolbar-wrapper">
         <div class="app-toolbar__btn-icon-group">
