af57f1c45baa48b9b870822db53f3604a35c32eb
Grigory <rehor.ger@gmail.com>
2023-02-07 09:24:05 +0100

    interface page init

--- src/core/routes/routers/ActionRouter.js
+++ src/core/routes/routers/ActionRouter.js
@@ -2,6 +2,10 @@ export class ActionRouter {
     constructor() {}
 
     static get path() {
+        return window.location;
+    }
+
+    static get param() {
         return window.location.hash.slice(1);
     }
 }
